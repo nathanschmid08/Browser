e6e4ce71034ff9dc532dc9ce075745fc5e2c4227
nathanschmid08 <nathandion.schmid@gmail.com>
2025-07-05 11:36:32 +0200

    restart new project

---
+++ main.js
@@ -0,0 +1,33 @@
+const { app, BrowserWindow } = require('electron');
+
+let mainWindow;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+        nodeIntegration: false,
+        contextIsolation: false,
+        enableRemoteModule: false,
+        webviewTag: true // ← das ist wichtig!      
+    }
+  });
+
+  // Lade unsere UI (index.html)
+  mainWindow.loadFile('index.html');
+
+  mainWindow.on('closed', function () {
+    mainWindow = null;
+  });
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') app.quit();
+});
+
+app.on('activate', function () {
+  if (mainWindow === null) createWindow();
+});
